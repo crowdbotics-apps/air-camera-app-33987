feaeea524d81bb3c6932813ecaa80f748f08074b
Crowdbotics <team@crowdbotics.com>
2022-03-04 22:10:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "air_camera_app_33987",
+  url: "https://air_camera_app_33987.botics.co",
+  api: "https://air_camera_app_33987.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
